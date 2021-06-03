dd459288d9d19c2f914b35777be28367287772f4
KTomoya-arch <kato.eng.dao@gmail.com>
2021-06-03 21:09:28 +0900

    sort

--- Sort_ruby.rb
+++ Sort_ruby.rb
@@ -1,10 +1,9 @@
 # 配列を定義
 lists = [10,8,3,5,2,4,11,18,20,33]
-
 # 範囲を指定
 range = 0..lists.length
 range.each do |i|
-  (i+1..lists.length-1).each do |k|
+  (i + 1..lists.length-1).each do |k|
     # 配列のi番目とk番目の大小を比較し並び替え
     if lists[i] > lists[k] then
       tempA = lists[i]
@@ -16,6 +15,3 @@ range.each do |i|
 end
 
 puts lists
-
-
-
